b44c158ca01242e15a9cf37298cb262da42ae28d
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2025-12-28 12:04:02 +0300

    Update and rename guery.sql to query.sql

--- 3/query.sql
+++ 3/query.sql
@@ -1,3 +1,15 @@
+-- AŞAMA 2: CEVAP:
+-- DEMOLAR
+SELECT * FROM Urunler ORDER BY urun_adi ASC;
+SELECT * FROM Urunler ORDER BY fiyat DESC;
+SELECT * FROM Urunler LIMIT 10;
+
+-- ÇÖZÜMLER
+SELECT * FROM Urunler ORDER BY fiyat ASC LIMIT 3;
+SELECT * FROM Urunler ORDER BY stok_adedi ASC;
+
+
+-- AŞAMA 3:
 -- [ÖĞRETMEN DEMOLARI]
 -- 1. Mağazada toplam kaç farklı ürün (satır) var? (COUNT)
 -- 2. Tüm dükkanın genel fiyat ortalaması nedir? (AVG)
@@ -7,3 +19,4 @@
 -- [ÖĞRENCİ GÖREVLERİ]
 -- GÖREV 1: Depo Hacmi: Mağazadaki tüm ürünlerin toplam stok adedini bulun (SUM).
 -- GÖREV 2: Kategori Analizi: Atıştırmalıklar (id=2) arasındaki EN PAHALI (MAX) ürünü bulun.
+
