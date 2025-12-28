8584e2deb48ebe2a16a9f782a6e3141e899b3ca3
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2025-12-28 12:02:10 +0300

    Update guery.sql

---
+++ 3/guery.sql
@@ -0,0 +1,9 @@
+-- [ÖĞRETMEN DEMOLARI]
+-- 1. Mağazada toplam kaç farklı ürün (satır) var? (COUNT)
+-- 2. Tüm dükkanın genel fiyat ortalaması nedir? (AVG)
+-- 3. Mağazadaki tüm ürünlerin TOPLAM stok miktarını görelim. (SUM)
+-- 4. Mağazadaki EN PAHALI ve EN UCUZ fiyatları görelim. (MAX/MIN)
+
+-- [ÖĞRENCİ GÖREVLERİ]
+-- GÖREV 1: Depo Hacmi: Mağazadaki tüm ürünlerin toplam stok adedini bulun (SUM).
+-- GÖREV 2: Kategori Analizi: Atıştırmalıklar (id=2) arasındaki EN PAHALI (MAX) ürünü bulun.
