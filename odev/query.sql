dba2827fc5a3c18de01f492fc013c7340ce158b9
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2025-12-28 12:56:34 +0300

    Update query.sql

---
+++ odev/query.sql
@@ -0,0 +1,51 @@
+-- ==========================================================
+-- 🕵️‍♂️ GÖREV: SİNEMA VERİ ANALİSTİ OPERASYONU
+-- Veritabanı: top250.db | Tablo: top250
+-- Amaç: Gelecek dersteki "Film Botu" projesi için veriyi tanı!
+-- ==========================================================
+
+
+-- ----------------------------------------------------------
+-- GÖREV 1: Arşivin En Eskileri
+-- Listedeki en eski (yılı en küçük) 5 filmi, isim ve yıl 
+-- bilgileriyle birlikte getirin.
+-- ----------------------------------------------------------
+-- BURAYA SQL KODUNU YAZIN:
+
+
+
+-- ----------------------------------------------------------
+-- GÖREV 2: Puan Şampiyonları
+-- IMDb puanı (rating) 9.0 ve üzerinde olan tüm efsanevi 
+-- filmleri listeleyin.
+-- ----------------------------------------------------------
+-- BURAYA SQL KODUNU YAZIN:
+
+
+
+-- ----------------------------------------------------------
+-- GÖREV 3: Tür Analizi (Raporlama)
+-- Hangi film türünden (genre) kaç adet film olduğunu 
+-- GROUP BY kullanarak özetleyin. 
+-- (En çok hangi türün sevildiğini bulalım!)
+-- ----------------------------------------------------------
+-- BURAYA SQL KODUNU YAZIN:
+
+
+
+-- ----------------------------------------------------------
+-- GÖREV 4: Finansal Bakış (Analiz)
+-- IMDb Top 250 listesindeki filmlerin ortalama bütçesini (budget) 
+-- ve toplam hasılatını (box_office) hesaplayın.
+-- ----------------------------------------------------------
+-- BURAYA SQL KODUNU YAZIN:
+
+
+
+-- ----------------------------------------------------------
+-- GÖREV 5: Uzun Metrajlılar
+-- Süresi (run_time) 180 dakikadan (3 saat) fazla olan 
+-- filmleri bulun.
+-- ----------------------------------------------------------
+-- BURAYA SQL KODUNU YAZIN:
+
