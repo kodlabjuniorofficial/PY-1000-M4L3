d8d1fc50424818fd1fd026b7f1f341fa32f1b8d2
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2025-12-28 12:01:49 +0300

    Update guery.sql

---
+++ 2/guery.sql
@@ -0,0 +1,8 @@
+-- [ÖĞRETMEN DEMOLARI]
+-- 1. Ürünleri A'dan Z'ye isim sırasına göre dizelim.
+-- 2. Ürünleri PAHALIDAN UCUZA (DESC) doğru sıralayalım.
+-- 3. Sadece ilk 10 ürünü görmeyi (LIMIT) öğrenelim.
+
+-- [ÖĞRENCİ GÖREVLERİ]
+-- GÖREV 1: En Ucuz 3 Ürün: Mağazadaki en ucuz 3 ürünü bulun (ASC ve LIMIT).
+-- GÖREV 2: Stok Alarmı: Ürünleri stok miktarına göre "Azdan Çoka" (ASC) doğru sıralayın.
