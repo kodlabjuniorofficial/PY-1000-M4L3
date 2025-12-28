058e03688f80bbd9f40c703b563d054975779238
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2025-12-28 12:02:28 +0300

    Update guery.sql

---
+++ 4/guery.sql
@@ -0,0 +1,7 @@
+-- [ÖĞRETMEN DEMOLARI]
+-- 1. Hangi kategoride kaç adet ürün çeşidi olduğunu özetleyelim.
+-- 2. Her kategorideki EN YÜKSEK (MAX) fiyatı görelim.
+
+-- [ÖĞRENCİ GÖREVLERİ]
+-- GÖREV 1: Kategori Fiyat Raporu: Her kategorinin karşısına ortalama fiyatı (AVG) yazdırın.
+-- GÖREV 2: Stok Dağılımı: Her kategoride toplam kaç adet STOK (SUM) olduğunu listeleyin.
